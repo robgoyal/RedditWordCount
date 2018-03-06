034ec2a888f36452d23f219c33ad1d541306abfa
Robin Goyal <goyal.rob@gmail.com>
2018-03-06 18:36:54 -0500

    Cleaned code for counting frequency of words from reddit submission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Name: main.py
 # Author: Robin Goyal
-# Last-Modified: March 4, 2018
+# Last-Modified: March 6, 2018
 # Purpose: Count the number of words in a reddit post
 
 import praw
@@ -21,6 +21,13 @@ def get_reddit_instance():
 
 
 def get_post(url):
+    """
+    (str) -> praw.Reddit.Submission
+
+    Returns an instance of the Submission object
+    from reddits API which contains information
+    for a submission from url.
+    """
 
     # Reddit instance
     reddit = get_reddit_instance()
@@ -32,52 +39,69 @@ def get_post(url):
 
 
 def get_freq_dict(submission):
+    """
+    (praw.Reddit.Submission) -> dict: (str, int)
 
-    # Initialize dictionary
+    Returns a dictionary containing the frequency
+    of words from a submission's title, body and comments.
+    """
+
     words_frequency = {}
-    total_words = 0
 
     # Remove all punctuation except for words and spaces
     regex = r'[^\w\s]|_'
 
-    # Update dictionary with title frequency
+    # Update words frequency dictionary from title
     title = re.sub(regex, '', submission.title)
-    total_words += update_frequency(words_frequency, title)
+    update_frequency(words_frequency, title)
 
-    # Update dictionary with body frequency
+    # Update words frequency dictionary from body
     soup = BeautifulSoup(submission.selftext, 'html.parser')
-    body = re.sub(regex, '', soup.get_text())
-    total_words += update_frequency(words_frequency, body)
 
-    # Update dictionary with all comments
+    # Remove html formatting from body
+    body = re.sub(regex, '', soup.get_text())
+    update_frequency(words_frequency, body)
+
+    # Update words frequency from all comments
     submission.comments.replace_more(limit=None)
     all_comments = submission.comments.list()
 
     for comment in all_comments:
-        comment = re.sub(regex, '', comment.body)
-        total_words += update_frequency(words_frequency, comment)
 
-    return total_words, words_frequency
+        # Replace punctuation from comments body
+        comment = re.sub(regex, '', comment.body)
+        update_frequency(words_frequency, comment)
+
+    return words_frequency
 
 
 def update_frequency(d, string):
-    num_words = 0
-    for word in string.split():
-        word = word.lower()
-        num_words += 1
-        if word in d:
-            d[word] += 1
-        else:
-            d[word] = 1
+    """
+    (dict: (str, int), str)
 
-    return num_words
+    Update dictionary, d with the frequency of
+    each word in string.
+    """
+
+    # Split string at all whitespace
+    for word in string.split():
+
+        # Lowercase word for consistency
+        word = word.lower()
+
+        # Increment frequency of word if exists in d
+        d[word] = d.get(word, 0) + 1
 
 
 def main():
-    submission = get_post("https://www.reddit.com/r/funny/comments/82ckwf/pic_of_two_plump_pigeons_perched_on_the_ledge_but/")
 
-    total_words, words_frequency = get_freq_dict(submission)
+    # Get total number of words and frequency of words in submission
+    post_url = input("URL of post submission: ")
+    submission = get_post(post_url)
+    words_frequency = get_freq_dict(submission)
+    total_words = sum(words_frequency.values())
 
+    # Print out information
     print(total_words)
     for k in sorted(words_frequency, key=words_frequency.get, reverse=True)[0:20]:
         print(k, words_frequency[k])
